0bf6058584ee33042cbd0481d55965757dd1feb3
root <root@anders.mikjaer.com>
2014-03-13 13:36:46 +0000

    Fixes and packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import mini
+import minipy
 
 try:
     from setuptools import setup
@@ -8,12 +8,10 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name=mini.__title__,
-    version=mini.__version__,
-    description=mini.__description__,
-    author=mini.__author__,
-
+    name=minipy.__title__,
+    version=minipy.__version__,
+    description=minipy.__description__,
+    author=minipy.__author__,
     url="http://aarvik.dk",
-    packages=['mini'],
-    package_dir={'mini': 'mini'},
-)
+    packages=['minipy'],
+    )
